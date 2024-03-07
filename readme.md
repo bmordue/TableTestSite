36f8cb2e52e7340c6ba10029269915f53f8bcab1
Ben Mordue <BMordue@hymans.co.uk>
2024-03-07 14:25:44 +0000

    complete demo

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Setup
+
+`npm install`
+
+# Running tests locally
+
+First:
+
+`cd www && npx serve -l 12345`
+
+Then:
+
+`cd test && npx cypress open`
---
+++ test/cypress/e2e/spec.cy.js
@@ -0,0 +1,12 @@
+describe("test site", () => {
+  it("check the details page for each row", () => {
+    cy.visit("http://localhost:12345");
+    cy.get("table").should("exist");
+
+    cy.get(`tbody > tr`).each(($row) => {
+      cy.wrap($row).find("a").click();
+      cy.contains("describes").should("be.visible");
+      cy.contains("Back").click();
+    });
+  });
+});
